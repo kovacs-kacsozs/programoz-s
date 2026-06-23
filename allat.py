a2c8ad8a63d71ce1e59c240ebb64403fcffc8bb2
kovacs-kacsozs <kovacs-kacsozs.24d@acsjszki.hu>
2026-06-23 10:07:24 +0200

    1.5

--- allat.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-class Állatfaj:
-    def __init__(self, fajnév, tömeg):
-        self.fajnév = fajnév
-        self.tömeg = tömeg
--- filmalap.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-def óraperc(): # Egésztse ki a függvénydefiníciót paraméterrel!
-    # Írja meg a függvény többi részét!
-    return str(óra) + ' óra ' + str(perc) + ' perc'
-
